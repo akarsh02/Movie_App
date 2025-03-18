21ccebf852a0aeaf10989e203c5e47732e99af18
akarsh <akarsh226@gmail.com>
2025-03-18 17:54:00 +0530

    added tailwindcss

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-black flex-1 justify-center items-center"
+      // style={{
+      //   flex: 1,
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      // }}
+    >
+<Text className=" text-red-500 ">Welcome</Text>
+    </View>
+  );
+}
